fafd518997376290e137f10d14923669a8e6f92d
Adhvaith Thurvas <adhvaith.thurvas@gmail.com>
2026-06-29 17:13:36 -0700

    more work on house predictions

--- prediction/house/02-house-poll-averages.R
+++ prediction/house/02-house-poll-averages.R
@@ -18,3 +18,16 @@ polls <- read_csv(url)
 setwd("../../")
 write_csv(polls, "2026_data/polls/house.csv")
 setwd("prediction/house")
+
+polls <- polls %>% filter(!(display_name %in% banned_pollsters)) %>%
+  filter((state != 'US') & (stage == 'general'))
+
+polls_pivot <- polls %>% pivot_wider(
+  id_cols = c(poll_id, question_id),
+  names_from = candidate_name,
+  values_from = pct
+)
+
+# All candidates who are confirmed to be competing in general
+confirmed_cands <- c("Anthony Constantino", "Blake Gendebien", "Cory Mills",
+                     )
