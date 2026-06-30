1e6d5cf5931767c62b4a558b526c501b62f7d3a6
Adhvaith Thurvas <adhvaith.thurvas@gmail.com>
2026-06-30 13:04:43 -0700

    fixed some things

---
+++ prediction/house/04-prediction.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(janitor)
+library(rstan)
+library(rstanarm)
+library(bayesplot)
+library(matrixStats)
+
+options(mc.cores = parallel::detectCores(logical = FALSE))
+
+data <- read_csv("transformed/2026_house_prediction_dataset.csv")
+
+data <- data %>% mutate(
+  dem_funds_2p_pct_sqrd = dem_funds_2p_pct**2,
+  effn = pmax(dem_effn, rep_effn),
+  sqrt_effn = sqrt(effn),
+  poll_margin = rep_poll_avg - dem_poll_avg # Keep consistency in convention
+)
+
