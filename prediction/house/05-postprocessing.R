e1a779b62d4ab5ac8f645708f0f5696166e9b5eb
Adhvaith Thurvas <adhvaith.thurvas@gmail.com>
2026-08-18 16:14:04 -0700

    updating model with new data, more postprocessing

--- prediction/house/05-postprocessing.R
+++ prediction/house/05-postprocessing.R
@@ -28,9 +28,15 @@ chamber_win_chance <- mean(tot_seats_sims > thres) * 100
 
 run_date <- today()
 
-output <- tibble(
-     date = as.Date(character()), y = double(), geo = character(), type = character()
-) ## Initial setup
+#output <- tibble(
+#     date = as.Date(character()), y = double(), geo = character(), type = character()
+#) ## Initial setup
+
+output <- read_csv("model_output/output_over_time.csv") # Update output_over_time.csv
+
+if (run_date %in% output$date) {
+  output <- output %>% filter(date != run_date)
+}
 
 output <- output %>% add_row(date = run_date, y = mean_seats_tot, geo = "US House", type = "seats") %>%
   add_row(date = run_date, y = chamber_win_chance, geo = "US House", type = "chance") %>%
@@ -47,3 +53,38 @@ seat_level_out <- data %>% select(cd, y_pred, y_pred_sd, chance) %>% mutate(
 output <- bind_rows(output, seat_level_out)
 
 write_csv(output, "model_output/output_over_time.csv")
+
+## Tipping point calculation
+
+postibble <- as_tibble(posterior)
+
+colnames(postibble) <- data$cd
+
+saveRDS(postibble, "model_output/labeled_posterior.RDS")
+
+#get_tipping_point <- function(sim_vector) {
+  ## sim_vector is a vector, presumably a row vector from posterior prediction matrix,
+  ## representing one full simulation of the House elections
+
+#  dem_winner <- sapply(sim_vector, \(x) x > 0)
+#  seats <- sum(dem_winner)
+  
+#  if (seats > thres) { # Democrats win in this sim
+#    sorted_vec <- sort(sim_vector, decreasing = FALSE)
+    
+#    winvec <- sorted_vec[sorted_vec > 0]
+#    seat_margin <- seats - thres
+#  }
+#  else { # Republicans win in this sim
+#    sorted_vec <- sort(sim_vector, decreasing = TRUE)
+    
+#    winvec <- sorted_vec[sorted_vec < 0]
+#    rep_seats <- dim(data)[1] - seats
+#    seat_margin <- rep_seats - (218 - dim(rep_uncont)[1])
+#  }
+#  return(winvec[seat_margin])
+#}
+
+#View(postibble %>% rowwise() %>% mutate(
+#  tipping_point_offset = list(get_tipping_point(c_across(everything())))
+#))
